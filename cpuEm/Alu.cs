3cfee07bcfeeca8ef7a1204338e051f19bdf061f
David Gulyas <gulyman@gmail.com>
2018-02-13 21:40:49 -0700

    Add special register behaviour to cpu

--- cpuEm/Alu.cs
+++ cpuEm/Alu.cs
@@ -4,18 +4,18 @@ namespace cpuEm
 {
 	public static class Alu
 	{
-		public static int calc(int op, int regA, int regB, int newPc, int oldPc)
+		public static int calc(int op, int arg1, int arg2, int newPc, int oldPc)
 		{
 			switch (op)
 			{
 				case 0:
-					return regA + regB;
+					return arg1 + arg2;
 				case 1:
-					return regA - regB;
+					return arg1 - arg2;
 				case 2:
-					return -1*regA;
+					return -1*arg1;
 				case 3:
-					return regA;
+					return arg1;
 				case 4:
 					//and
 					throw new Exception($"Operation And (num 4) is not supported");
@@ -24,29 +24,28 @@ namespace cpuEm
 					throw new Exception($"Operation Or (num 5) is not supported");
 				case 6:
 					//boolean inverter
-					return regA == 0 ? 1 : 0; 
+					return arg1 == 0 ? 1 : 0; 
 				case 7:
 					//greater than
-					return regA > regB ? 1 : 0;
+					return arg1 > arg2 ? 1 : 0;
 				case 8:
 					//equal
-					return regA == regB ? 1 : 0;
+					return arg1 == arg2 ? 1 : 0;
 				case 9:
 					//less than
-					return regA < regB ? 1 : 0;
+					return arg1 < arg2 ? 1 : 0;
 				case 10:
 					//jump if >
-					return regA > regB ? newPc : oldPc + 1;
+					return arg1 > arg2 ? newPc : oldPc + 1;
 				case 11:
 					//jump if =
-					return regA == regB ? newPc : oldPc + 1;
+					return arg1 == arg2 ? newPc : oldPc + 1;
 				case 12:
 					//jump if <
-					return regA < regB ? newPc : oldPc + 1;
+					return arg1 < arg2 ? newPc : oldPc + 1;
 				default:
 					throw new Exception($"Operation number {op} is not supported");
 			}
-
 		}
 
 	}
--- cpuEm/cpu.cs
+++ cpuEm/cpu.cs
@@ -8,14 +8,23 @@ namespace cpuEm
 		public List<Instruction> Instructions;
 		public List<int> ConsoleBuffer = new List<int>();
 
+		private List<int> Ram = new List<int>();
+
 		private const int PcReg = 15; //The last register is the program counter.
 		private const int ConsoleReg = 14; //Writing to this reg writes to the console.
+		private const int LiteralReg = 14; //Reading from this reg reads the literal in the instruction
+		private const int RamReg = 13; //Reading and writing to this reg acesses the ram
+		private const int RamAddressReg = 12;// Normal reg, but also the ram address that's currently accessed
 
 		public void Tick()
 		{
 			var ci = GetCurrentInstruction();
+			Regs[LiteralReg] = ci.Literal;
+			Regs[RamReg] = Ram[Regs[RamAddressReg]];
 
-			Regs[ci.DestReg] = Alu.calc(ci.Function, ci.RegA, ci.RegB, ci.NewPcValue, Regs[PcReg]);
+			Regs[ci.DestReg] = Alu.calc(ci.Function, Regs[ci.RegA], Regs[ci.RegB], Regs[ci.NewPcValue], Regs[PcReg]);
+
+
 			if (ci.DestReg == ConsoleReg)
 			{
 				ConsoleBuffer.Add(Regs[ci.DestReg]);
